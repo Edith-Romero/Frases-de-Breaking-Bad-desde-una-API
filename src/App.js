15f602e45b3f7193855bfeea39566bc083ac7e29
Edith Romero <edithjohanaromero@gmail.com>
2021-08-22 11:31:21 -0300

    Proyect Completo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import React, {useState,useEffect} from 'react';
+import styled from '@emotion/styled';
+import Frases from './Components/Frases'
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 10rem;
+  flex-direction: column;
+`;
+
+const Boton = styled.button `
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border:4px solid blank;
+  transition: background-size .8s ease;
+
+  :hover{
+    cursor:pointer;
+    background-size: 800px;
+  }
+`;
 
 function App() {
+
+  const [frase, setFrase] = useState({});
+
+  // Forma de hacerlo con promise / resolver
+// const ConsultarApi = () => {
+//   const api = fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+//   const frase = api.then(respuesta => respuesta.json());
+//   frase.then (resultado => console.log(resultado));
+// }
+
+// Forma de hacerlo con el async
+const ConsultarApi = async () => {
+  const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+  const frase = await api.json()
+  setFrase(frase[0]);
+}
+
+// Cargar una frase apenas carga la pagina
+
+useEffect(()=>{
+  ConsultarApi();
+},[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Contenedor>
+        <Frases
+          frase={frase}        
+        />
+        <Boton
+          onClick={ConsultarApi}>
+          Obtener Frase
+        </Boton>
+    </Contenedor>
   );
 }
 
